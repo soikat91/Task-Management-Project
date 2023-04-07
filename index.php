a663f4ac6734e64176e49e9b0e9e7764bda1c0ae
soikat <soikateman@gmail.com>
2023-04-07 16:53:50 +0600

    added task management project

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3">
+
+        </div>
+        <div class="col-lg-6">
+            <h1>
+                Task Manager
+            </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, temporibus.</p>
+
+            <h3>All Tasks</h3>
+
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <td>Id</td>
+                        <td>Task</td>
+                        <td>Date</td>
+                        <td>Action</td>
+                    </tr>
+                </tbody>
+            </table>
+
+        <h2>Add Tasks</h2>
+            <form action="">
+                <label class="form-label" for="">Task</label>
+                <input class="form-control" type="text" name="text" id="">
+                <label class="form-label" for="">Date</label>
+                <input  class="form-control" type="text" name="date" id="">
+                <button type="submit" class="btn btn-primary mt-3">Add</button>
+            </form>
+        </div>
+        <div class="col-lg-3">
+            
+        </div>
+    </div>
+
+
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
+</body>
+</html>
